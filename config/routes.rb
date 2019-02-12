1b7881c3a2e7ef4e2bf97a18eda0ea9d77cc0884
Mauricio Homma <mhhomma@gmail.com>
2019-02-12 18:21:31 -0200

    Task manager working

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
-  get 'tasks/new', to: 'tasks#new'
   post 'tasks', to: 'tasks#create'
-  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  get 'tasks/new', to: 'tasks#new'
   get 'tasks/:id/edit', to: 'tasks#edit', as: 'tasks_edit'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
   patch 'tasks/:id', to: 'tasks#update'
   delete 'tasks/:id', to: 'tasks#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
