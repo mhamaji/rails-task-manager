bae5be220b8cf23292147451699fd8c45834ff14
Mauricio Homma <mhhomma@gmail.com>
2019-02-12 16:14:36 -0200

    Fixed convention

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
-  get 'task/:id', to: 'tasks#show', as: 'task'
-  get 'task/new', to: 'tasks#new'
-  post 'task', to: 'tasks#create'
-  get 'task/:id/edit', to: 'tasks#edit', as: 'task_edit'
-  patch 'task/:id', to: 'tasks#update'
-  delete 'task/:id', to: 'tasks#destroy'
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'tasks_edit'
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
